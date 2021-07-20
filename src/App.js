d36e66fceb59564c9015763483abbb2641c683fb
David Vasek <david.vasek51@gmail.com>
2021-07-20 11:07:59 -0500

    create Hello component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Hello from "./components/Hello";
+
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Hello name="David" />
+    </div>
+  );
 }
---
+++ src/components/Hello.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hello = (props) => {
+  return (
+    <h1 h1 className="greeting">
+      Hello {props.name}!
+    </h1>
+  );
+};
+
+export default Hello;
